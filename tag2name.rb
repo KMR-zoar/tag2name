cc89732335982216010d1f3074511d2d820a5510
Zoar <zoar@k-side.net>
2016-11-25 09:35:38 +0900

    MP3のタグを表示する機能まで実装

---
+++ tag2name.rb
@@ -0,0 +1,22 @@
+#!/usr/bin/env ruby
+# coding: utf-8
+
+require 'bundler'
+Bundler.require
+
+def getTaginfo(file)
+   Mp3Info.open(file) do |mp3|
+      title = mp3.tag2.TIT2
+      artist = mp3.tag2.TPE1
+      tag = [artist, title]
+      return tag
+   end
+end
+
+Dir.chdir(ARGV[0])
+
+files=Dir.glob("*.mp3")
+
+files.each do |file|
+   puts getTaginfo(file)
+end
